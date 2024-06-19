736e17963135d14fcafd1cb27ff2120a7a3e58a6
Samokhov Artem <artemsamokhov2@gmail.com>
2024-06-19 12:38:12 +0300

    Updates

---
+++ assets/js/fdcfe3c8.823e089a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[7183],{130:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/my-notes-with-docusaurus/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
